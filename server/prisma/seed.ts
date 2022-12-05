e7c4b50742edb1681b6e18c9ab7f7eefadba724f
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2022-12-05 01:06:12 -0300

    NOVO: Conclusão da segunda aula

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'DBas',
+            email: 'DBas@gmail.com',
+            avatarUrl: 'https://github.com/eduardoangelosimon.png'
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-24T12:00:00.400Z',
+            firstTeamCountyCode: 'DE',
+            secondTeamCountryCode: 'BR',
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-13T19:21:57.400Z',
+            firstTeamCountyCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+main()
